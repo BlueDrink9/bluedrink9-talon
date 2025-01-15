dacf5a98c7437f9b4b15fe0c33fe604c434346f0
bluedrink9 <bluedrink9@users.noreply.github.com>
2025-01-15 14:07:51 +1300

    Use custom capture to extend symbol keys

---
+++ symbol_key.py
@@ -0,0 +1,16 @@
+from talon import Module, Context
+
+mod = Module()
+mod.list("my_symbol_key", desc="extra symbol keys")
+
+ctx = Context()
+ctx.lists["user.my_symbol_key"] = {
+    "lend": "(",
+    "rend": ")",
+    "face": "}",
+    "rare": "]",
+}
+
+@ctx.capture("user.symbol_key", rule="{user.symbol_key} | {user.my_symbol_key}")
+def symbol_key(m):
+    return str(m)
