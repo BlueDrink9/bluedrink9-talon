4d6c85da03d67d796359496664072b5c14703a6f
bluedrink9 <bluedrink9@users.noreply.github.com>
2025-01-31 20:16:03 +1300

    noise map update?

--- noise_map.py
+++ noise_map.py
@@ -1,70 +1,73 @@
-# from talon import Module, actions, noise
-#
-# noise_module = Module()
-#
-# @noise_module.action_class
-# class NoiseActions:
-#     def noise_pop():
-#         """Invoked when the user does the pop noise."""
-#         pass
-#
-#     def noise_hiss_start():
-#         """Invoked when the user starts hissing (potentially while speaking)"""
-#         pass
-#
-#     def noise_hiss_stop():
-#         """Invoked when the user finishes hissing (potentially while speaking)"""
-#         pass
-#
-#
-# def hiss_handler(active):
-#     if active:
-#         pass
-#         # actions.key('shift')
-#
-# def on_pop(active):
-#     pass
-#
-# noise.register("hiss", hiss_handler)
-# noise.register("pop", on_pop)
-# script cursor scrolling while hissing
-from talon import actions, ctrl, noise, cron
+from talon import Module, actions, noise, ctrl, cron
 from time import sleep, time
+
+noise_module = Module()
+
+noise_length_threshold = 500 # ms
 
 start = 0
 running = False
-noise_length_threshold = 500 # ms
 threshold_passed = False
 
-def still_running():
-    global running
-    global threshold_passed
-    if running:
-        print('hiss duration passed threshold, starting gaze scroll')
-        threshold_passed = True
-        actions.user.mouse_scroll_down_continuous()
+@noise_module.action_class
+class NoiseActions:
 
-def cursor_scroll_on_hiss(is_active):
-  global start
-  global running
-  global threshold_passed
-  if is_active:
-    start = time()
-    # Only if this is a continuous hiss. If already running the timer,
-    # don't start a new one.
-    if not running:
-      running = True
-      # Monitor for continuous hissing. Each hiss will keep running this
-      # function.
-      # for interval in range(100, noise_length_threshold, 100):
-      for interval in [noise_length_threshold]:
-        cron.after(str(interval) + "ms", still_running)
-  else:
-    running = False
-    if threshold_passed:
+    # def noise_pop():
+    #     """Invoked when the user does the pop noise."""
+    #     pass
+
+    def noise_hiss_start():
+        """Invoked when the user starts hissing (potentially while speaking)"""
+        NoiseActions.cursor_scroll_on_hiss(True)
+
+    # script cursor scrolling while hissing
+    def still_hissing():
+        """If we haven't stopped hissing since starting the timer, start scrolling down"""
+        global running
+        global threshold_passed
+        global start
+        if running and time() - start > noise_length_threshold:
+            print('hiss duration passed threshold, starting gaze scroll')
+            threshold_passed = True
+            actions.user.mouse_scroll_down_continuous()
+
+    def cursor_scroll_on_hiss(is_active: bool):
+        """Start timer that will initiate cursor scrolling on hiss, if the hiss length passes the threshold"""
+        global start
+        global running
+        global threshold_passed
+        # Only if this is a continuous hiss. If already running the timer,
+        # don't start a new one.
+        if running:
+            return
+        start = time()
+        running = True
+        print('Starting a hiss scroll timer')
+        for interval in [noise_length_threshold]:
+            cron.after(str(interval) + "ms", NoiseActions.still_hissing)
+
+    def cursor_hiss_scroll_stop():
+        """Stop the hiss running - indicate it was not continuous, and stop scrolling if currently doing so"""
+        global running
+        global threshold_passed
+        running = False
         print('end of hiss detected, disabling gaze scroll')
         threshold_passed = False
         actions.user.mouse_scroll_stop()
 
+    def noise_hiss_stop():
+        """Invoked when the user finishes hissing (potentially while speaking)"""
+        NoiseActions.cursor_hiss_scroll_stop()
 
-noise.register('hiss', cursor_scroll_on_hiss)
+
+def hiss_handler(active):
+    if active:
+        NoiseActions.noise_hiss_start
+    else:
+        NoiseActions.noise_hiss_stop
+
+def on_pop(active):
+    pass
+
+# noise.register("hiss", hiss_handler)
+# noise.register("pop", on_pop)
