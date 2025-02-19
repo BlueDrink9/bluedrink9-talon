12c2d4ee7bea981e4730cf4be10656c8f3993686
bluedrink9 <bluedrink9@users.noreply.github.com>
2025-02-19 16:42:44 +1300

    configure dolphin

---
+++ apps/dolphin/dolphin.py
@@ -0,0 +1,48 @@
+from talon import Context, Module, actions, clip, ui
+
+# App definition
+mod = Module()
+mod.apps.dolphin = """
+os: linux
+and app: dolphin
+"""
+
+# Context matching
+ctx = Context()
+ctx.matches = r"""
+app: dolphin
+"""
+
+
+@ctx.action_class("user")
+class UserActions:
+    # user.tabs
+    def tab_jump(number: int):
+        actions.key(f"alt-{number}")
+
+    # user.navigation
+    def go_back():
+        actions.key("alt-left")
+
+    def go_forward():
+        actions.key("alt-right")
+
+    # user.file_manager
+    def file_manager_open_parent():
+        actions.key("alt-up")
+
+    def file_manager_show_properties():
+        actions.key("alt-enter")
+
+    def file_manager_open_directory(path: str):
+        actions.key("ctrl-l")
+        actions.insert(path)
+        actions.key("enter")
+
+    def file_manager_new_folder(name: str = None):
+        actions.key("ctrl-shift-n")
+        if name:
+            actions.insert(name)
+
+    def file_manager_terminal_here():
+        actions.key("alt-shift-f4")
